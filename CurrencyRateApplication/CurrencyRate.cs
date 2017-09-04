78ec3226bda1eac84ee72d88b71f111eddca5d4b
NikitinNikita <nikitin.nikitaa@outlook.com>
2017-09-04 16:43:14 +0300

    Currency Rate Application
    Simple WPF Application for showing Currency Rate

---
+++ CurrencyRateApplication/CurrencyRate.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+
+namespace CurrencyRateApplication
+{
+    /// <summary>
+    /// Класс для получения информации о текущем курсе рубля.
+    /// Данные берутся с сайта Центрального банка РФ
+    /// </summary>
+    class CurrencyRate
+    {
+        /// <summary>
+        /// Список валют, считанный с сайта Центрального банка РФ
+        /// </summary>
+        private List<Currency> DataFromCBR;
+
+        /// <summary>
+        /// Инициализация нового экземпляра класса CurrencyRate
+        /// </summary>
+        public CurrencyRate()
+        {
+            // Получаем (считываем) данные с сайта Центрального банка РФ
+            GetCurrencyRateFromCBR();
+        }
+
+        /// <summary>
+        /// Получить (считать) данные с сайта Центрального банка РФ
+        /// Результат записывается в поле DataFromCBR класса CurrencyRate
+        /// </summary>
+        public void GetCurrencyRateFromCBR()
+        {
+            // Создаём объект класса XmlDocument
+            XmlDocument xmlDoc = new XmlDocument();
+
+            // Загружаем XML документ с сайта Центрального банка РФ
+            xmlDoc.Load("http://www.cbr.ru/scripts/XML_daily.asp");
+
+            // Получаем корневой элемент документа
+            XmlElement root = xmlDoc.DocumentElement;
+
+            // Получаем список тэгов Valute
+            XmlNodeList nodes = root.SelectNodes("/ValCurs/Valute");
+
+            // Создаём новый список валют (объектов класса Currency)
+            this.DataFromCBR = new List<Currency>();
+
+            // Для каждого тэга Valute
+            foreach (XmlNode node in nodes)
+            {
+                // Получаем текст тэга Name - название валюты
+                string name = node["Name"].InnerText;
+
+                // Получаем текст тэга Nominal - номинал валюты
+                string nominal = node["Nominal"].InnerText;
+
+                // Получаем текст тэга CharCode - аббревиатура валюты (код)
+                string code = node["CharCode"].InnerText;
+
+                // Получаем текст тэга Value - курс (значение) валюты
+                double value = Convert.ToDouble(node["Value"].InnerText);
+
+                // Создаём объект класса Currency, передавая в качестве аргументов название, код, номинал и значение курса валюты
+                // и добавляем полученный экземпляр в список
+                this.DataFromCBR.Add(new Currency(name, code, nominal, value));
+            }
+        }
+
+        /// <summary>
+        /// Получить список названий валют
+        /// </summary>
+        /// <returns>
+        /// Возвращает список пар ключ-значение, где ключом является аббериватура валюты (EUR, USD и т.д.), 
+        /// а значением - описание валюты на русском языке (например, 'Доллар США')
+        /// </returns>
+        public List<KeyValuePair<string, string>> GetCurrencies()
+        {
+            // Создаём список пар ключ-значение
+            var currencies = new List<KeyValuePair<string, string>>();
+            
+            // Для каждой валюты из списка валют, считанный с сайта Центрального банка РФ
+            foreach(Currency currency in this.DataFromCBR)
+            {
+                // Создаём пару ключ-значение, где , где ключом является аббериватура валюты (EUR, USD и т.д.), 
+                // а значением - описание валюты на русском языке
+                var currencyPair = new KeyValuePair<string, string>(currency.CharCode, currency.Name);
+
+                // Добавляем созданную пару в список валют
+                currencies.Add(currencyPair);
+            }
+
+            // Возрващаем полученный список
+            return currencies;
+        }
+
+        /// <summary>
+        /// Получить курс (значение) валюты по коду (аббревиатуре)
+        /// </summary>
+        /// <param name="CharCode">Международный код валюты</param>
+        /// <returns>Строка в следующем формате: 'Номинал Название_валюты = Значение_валюты руб.'</returns>
+        public string GetCurrencyRateByCode(string CharCode)
+        {
+            string rate = (from currency in this.DataFromCBR // для каждой валюты из DataFromCBR
+                           where currency.CharCode == CharCode // где код текущей валюты равен полученному коду 
+                           select currency.Nominal + " " + currency.Name + " = " + currency.Value + " руб." // Выбираем строку согласно шаблону
+                           ).First(); // Из полученных результатов выбираем первый
+
+            // Возрващаем полученный курс
+            return rate;
+        }
+    }
+
+    /// <summary>
+    /// Класс для хранения и представления валюты
+    /// </summary>
+    class Currency
+    {
+        /// <summary>
+        /// Название валюты
+        /// </summary>
+        public string Name;
+
+        /// <summary>
+        /// Аббревиатура валюты (международный код)
+        /// </summary>
+        public string CharCode;
+
+        /// <summary>
+        /// Номинал валюты
+        /// </summary>
+        public string Nominal;
+
+        /// <summary>
+        /// Текущий курс
+        /// </summary>
+        public double Value;
+
+        /// <summary>
+        /// Инициализация нового экземпляра класса Currency
+        /// </summary>
+        /// <param name="Name_">Название валюты</param>
+        /// <param name="CharCode_">Международный код</param>
+        /// <param name="Nominal_">Номинал валюты</param>
+        /// <param name="Value_">Текущий курс</param>
+        public Currency(string Name_, string CharCode_, string Nominal_, double Value_)
+        {
+            this.Name = Name_;
+
+            this.CharCode = CharCode_;
+
+            this.Nominal = Nominal_;
+
+            this.Value = Value_;
+        }
+    }
+}
---
+++ CurrencyRateApplication/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace CurrencyRateApplication
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Словарь (список пар ключ-значение) валют.
+        /// В качестве ключа выступает аббревиатура (междунардный код) валюты,
+        /// в качестве значения - название валюты на русском языке.
+        /// </summary>
+        private List<KeyValuePair<string, string>> Сurrencies;
+
+        /// <summary>
+        /// Экземпляр класса CurrencyRate для работы с курсом валют с сайта Центрального банка РФ
+        /// </summary>
+        private CurrencyRate CurrencyRateFromCBR;
+
+        /// <summary>
+        /// Индекс выбранной пользователем валюты
+        /// </summary>
+        private int SelectedIndex = 0;
+
+        /// <summary>
+        /// Код выбранной пользователем валюты
+        /// </summary>
+        private string SelectedRateCode = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                // Получаем список валют и устанавливаем названия валют в ComboBox
+                InitializeForm();
+            }
+            catch // Если в процессе получения курса валют произошла ошибка,
+            {
+                // То отображаем пользователю сообщение об ошибке
+                MessageBox.Show("Во время получения курса валют произошла ошибка. Программа будет закрыта", "Ошибка получения курса валют", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                // Закрываем программу
+                this.Close();
+            }
+        }
+
+        /// <summary>
+        /// Инициализация формы - получение списка валют и установка названий валют в ComboBox
+        /// </summary>
+        private void InitializeForm()
+        {
+            // Создаём экземпляр класса CurrencyRate
+            this.CurrencyRateFromCBR = new CurrencyRate();
+
+            // Получаем словарь названий валют
+            this.Сurrencies = this.CurrencyRateFromCBR.GetCurrencies();
+
+            // Создаём список валют, который отобразятся пользователю в выпадающем меню
+            List<string> comboBoxItems = new List<string>();
+
+            // Для каждой пары ключ-значение из словаря валют
+            foreach (KeyValuePair<string, string> currencyPair in this.Сurrencies)
+            {
+                // Добавляем название валюты в список отображаемых названий
+                comboBoxItems.Add(currencyPair.Value);
+            }
+
+            // Устанавливаем полученный список валют как источник данных для ComboBox 
+            this.comboBoxCurrency.ItemsSource = comboBoxItems;
+
+            // Устанавливаем текущую валюту 
+            this.comboBoxCurrency.SelectedIndex = this.SelectedIndex;
+        }
+
+        /// <summary>
+        /// Метод, вызываемый при изменении текущей валюты в ComboBox
+        /// </summary>
+        private void comboBoxCurrency_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Запоминаем индекс выбранной валюты
+            this.SelectedIndex = this.comboBoxCurrency.SelectedIndex;
+
+            // Получаем название выбранной пользователем валюты
+            string selectedName = this.comboBoxCurrency.Items[this.SelectedIndex].ToString();
+
+            this.SelectedRateCode = (from currencyPair in this.Сurrencies // Для каждой пары из словаря валют
+                                    where currencyPair.Value == selectedName // где значение пары равно выбранному имени валюты
+                                    select currencyPair.Key // выбираем ключ - код валюты
+                                    ).First(); // Из полученной коллекции выбираем первый элемент
+
+            // Устанавливаем актуальный курс выбранной валюты
+            SetActualRate();
+        }
+
+        /// <summary>
+        /// Метод, вызываемый при нажатии на кнопку "Обновить курс"
+        /// Обновляет курс текущей валюты, а также обновляет список валют
+        /// </summary>
+        private void buttonRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            // Получаем список валют и устанавливаем названия валют в ComboBox
+            InitializeForm();
+
+            // Устанавливаем актуальный курс выбранной валюты
+            SetActualRate();
+        }
+
+        /// <summary>
+        /// Установить (показать пользователю) актуальный курс выбранной валюты
+        /// </summary>
+        private void SetActualRate()
+        {
+            // Получаем текущий курс согласно выбранному коду валюты
+            string actualRate = this.CurrencyRateFromCBR.GetCurrencyRateByCode(this.SelectedRateCode);
+
+            // Выводим пользователю актуальный курс выбранной валюты
+            this.textBoxCurrentRate.Text = actualRate;
+        }
+    }
+}
